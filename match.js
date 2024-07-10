83921a4f342b2dfba2fd3eb27a8db14043e2f22a
Himanshu_full_stack_developer <158418021+HIMANSHUFULLSTACKDEVELOPER@users.noreply.github.com>
2024-07-10 09:14:16 +0530

    Add files via upload

--- match.js
+++ match.js
@@ -1,12 +1,12 @@
 let lastTeam = localStorage.getItem('lastTeam') || "";
-
+// team
 const teamLogos = {
     'MI': 'https://upload.wikimedia.org/wikipedia/en/thumb/c/cd/Mumbai_Indians_Logo.svg/1200px-Mumbai_Indians_Logo.svg.png',
     'RCB': 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUfpDPZ5AwBAPHlCvq7CrA4guqST4CaBAOYw&s',
     'CSK': 'https://upload.wikimedia.org/wikipedia/en/thumb/2/2b/Chennai_Super_Kings_Logo.svg/1200px-Chennai_Super_Kings_Logo.svg.png',
     'KKR': 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Kolkata_Knight_Riders_Logo.svg/1200px-Kolkata_Knight_Riders_Logo.svg.png'
 };
-
+// select team
 function selectTeam(team) {
     const matchDisplay = document.getElementById("match-display");
     const teamLogo = document.getElementById("team-logo");
@@ -28,7 +28,7 @@ function selectTeam(team) {
         matchDisplay.textContent = "Please select a different team";
     }
 }
-
+// get js
 function getRandomOpponent(team) {
     const teams = Object.keys(teamLogos).filter(t => t !== team);
     return teams[Math.floor(Math.random() * teams.length)];
